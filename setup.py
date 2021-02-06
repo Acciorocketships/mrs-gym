3c45f8cd1d7b95d6f7b28ac8dbf4fcbf6613a553
Ryan Kortvelesy <rk627@cam.ac.uk>
2021-02-06 20:14:03 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup
 
 setup(name='mrsgym',
-      version='0.0.1',
+      version='1.0.0',
       install_requires=['gym','scipy','torch','pybullet','numpy'],
-      include_package_data=True
+      include_package_data=True,
+      author = 'Ryan Kortvelesy',
+      author_email = 'rk627@cam.ac.uk',
+      description = 'A pybullet-based multiagent simulator',
 )
